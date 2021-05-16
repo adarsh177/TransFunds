9092625837030c7e5d3c0320b69e36a3614cef4e
Adarsh <adarsh1772000@gmail.com>
2021-05-16 19:08:16 +0530

    changes

--- transfunds-ngo/src/screens/Dashboard/Dashboard.js
+++ transfunds-ngo/src/screens/Dashboard/Dashboard.js
@@ -7,8 +7,78 @@ import NgoDetailCardDashboard from "../../components/NgoDetailCardDashboard/NgoD
 import { Scrollbars } from "react-custom-scrollbars";
 
 import "./Dashboard.css";
+import Utils from "../../Utils/Utils";
 
 class Dashboard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      details: {},
+      availableDonation: 0,
+      totalDonation: 0,
+      myDonations: []
+    }
+  }
+
+  componentDidMount(){
+    if(window.accountId == null){
+      this.props.history.push('/');
+      return;
+    }
+
+    if(Object.keys(this.state.details) == 0){
+      this.loadData();
+    }
+  }
+
+  loadData(){
+    Utils.getNGODetail(window.ngoId, (err, details) => {
+      if(err){
+        alert('Error loading details');
+      }else{
+        this.setState({details: details});
+        this.forceUpdate();
+      }
+    });
+
+    Utils.getTotalDonation((total) => {
+      this.setState({totalDonation: total ? total : 0});
+    });
+
+    Utils.getAvailableDonation((av) => {
+      this.setState({availableDonation: av ? av : 0});
+    });
+
+    this.loadMyDonations();
+  }
+
+  loadMyDonations(){
+    window.web3.eth.getBlockNumber()
+      .then(bn => {
+        window.contract.getPastEvents('Transaction', {
+          fromBlock: bn-500,
+          toBlock: 'latest'
+        }, (err, events) => {
+          console.log(err, events);
+          if(err == null){
+            console.log('Got events', events);
+            this.state.myDonations = [];
+            events.forEach(event => {
+              this.state.myDonations.push({
+                  to: event.returnValues.to,
+                  from: event.returnValues.from,
+                  amount: event.returnValues.amount,
+              });
+            });
+          }else{
+            console.log('Error getting events', err);
+          }
+          this.setState({loaded: true});
+        });
+        console.log('Current block', bn);
+      });
+  }
+
   render() {
     return (
       <div className="MainContainer">
@@ -16,68 +86,38 @@ class Dashboard extends Component {
           <Topbar title="NGO Dashboard" />
           <div className="NgoDetailCardDashboard-wrapper">
             <NgoDetailCardDashboard
-              name="hemkunt Foundation"
-              ngoid="5646963"
-              email="example@ez.com"
-              phone="9876543211"
+              name={this.state.details.name}
+              ngoid={window.ngoId}
+              email={this.state.details.email}
+              phone={this.state.details.mobile}
             />
           </div>
           <SubHeading value="Transactions" />
           <div className="Transaction-container">
             <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={200}>
-              <TransactionCard
-                username="Aman Kumar"
-                tid="354343"
-                pamount="+ ₹551"
-              />
-              <TransactionCard
-                username="Aman Kumar"
-                tid="354343"
-                pamount="+ ₹551"
-              />
-              <TransactionCard
-                username="Aman Kumar"
-                tid="354343"
-                pamount="+ ₹551"
-              />
-              <TransactionCard
-                username="Aman Kumar"
-                tid="354343"
-                reasonParameter="Reason - "
-                reason="Food, Cloth"
-                namount="- ₹551"
-              />
-              <TransactionCard
-                username="Aman Kumar"
-                tid="354343"
-                pamount="+ ₹551"
-              />
-              <TransactionCard
-                username="Aman Kumar"
-                tid="354343"
-                pamount="+ ₹551"
-              />
-              <TransactionCard
-                username="Aman Kumar"
-                tid="354343"
-                reasonParameter="Reason - "
-                reason="Food, Cloth"
-                namount="- ₹551"
-              />
+              {this.state.myDonations.map(donation => {
+                return(
+                  <TransactionCard
+                    username={donation.from}
+                    tid={window.ngoId}
+                    pamount={donation.amount + " WEI"}
+                  />
+                );
+              })}
             </Scrollbars>
           </div>
           <div className="Fixed-bottom-wrapper">
             <div className="DonationsStats">
               <div className="stats-container">
-                <p className="StatsParameter"> Total - </p>
-                <p className="statsValue">₹51515</p>
+                <p className="StatsParameter"> Total</p>
+                <p className="statsValue">{this.state.totalDonation} WEI</p>
               </div>
               <div className="stats-container">
-                <p className="StatsParameter">Amount Left -</p>
-                <p className="statsValue">₹51515</p>
+                <p className="StatsParameter">Available </p>
+                <p className="statsValue">{this.state.availableDonation} WEI</p>
               </div>
             </div>
-            <span class="middleLine"></span>
+            <span className="middleLine"></span>
             <div>
               <Button value="Withdraw" />
             </div>
