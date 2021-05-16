65ffbd8a847258e86100e33ad44bcf8f97724bda
Adarsh <adarsh1772000@gmail.com>
2021-05-16 14:32:22 +0530

    started integration

--- transfunds-user/src/screens/SplashScreen/SplashScreen.js
+++ transfunds-user/src/screens/SplashScreen/SplashScreen.js
@@ -1,11 +1,43 @@
 import React from "react";
+import Portis from '@portis/web3';
+import Web3 from 'web3';
 import { withRouter } from "react-router-dom";
 import Logo from "../../resources/logo.png";
 import Loder from "../../resources/Loader.gif";
 import PortisLogo from "../../resources/portis_logo.png";
 import "./SplashScreen.css";
+import config from '../../config.json';
 
 class SplashScreen extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+    if(window.portis && window.web3 && window.accountId){
+      this.moveForward();
+    }else{
+      this.initWeb3AndPortis();
+    }
+  }
+
+  initWeb3AndPortis(){
+    window.portis = new Portis(config.portis_app_id,{
+      nodeUrl: config.nodeUrl,
+      chainId: config.chainId
+  });
+    window.web3 = new Web3(window.portis.provider);
+    window.web3.eth.getAccounts((err, acounts) => {
+      window.accountId = acounts[0];
+      console.log(err, acounts);
+      this.moveForward();
+    })
+  }
+
+  moveForward(){
+    setTimeout(() => this.props.history.push('/Dashboard'), 2000);
+  }
+
   render() {
     return (
       <div className="MainContainer">
