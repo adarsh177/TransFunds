b7b2f1f0d67e02a84864c35e8bc9ffa331f26ede
Adarsh <adarsh1772000@gmail.com>
2021-05-16 07:12:58 +0530

    fixes

--- transfunds-user/src/components/NgoListCard/NgoListCard.js
+++ transfunds-user/src/components/NgoListCard/NgoListCard.js
@@ -1,30 +1,25 @@
 import React, { Component } from "react";
-import { withRouter } from "react-router";
 import "./NgoListCard.css";
 
-class NgoListCard extends Component {
-  render() {
-    return (
-      <div>
-        <div className="DashboardCard-wrapper">
-          <div>
-            <img className="ngo-logo" src={this.props.source} alt="Ngo logo" />
-          </div>
-          <div className="card-content">
-            <h2 className="ngoName">{this.props.ngoname}</h2>
-            <p className="allTransactionDetails">
-              NGO id - #{this.props.ngoid}
-            </p>
-            <p className="allTransactionDetails">
-              Location - ₹ {this.props.location}
-            </p>
-            <p className="allTransactionDetails">Field - {this.props.field}</p>
-          </div>
+export default function NgoListCard(props){
+  return (
+    <div>
+      <div className="DashboardCard-wrapper">
+        <div>
+          <img className="ngo-logo" src={props.source} alt="Ngo logo" />
         </div>
-        <span className="cardLine"></span>
+        <div className="card-content">
+          <h2 className="ngoName">{props.ngoname}</h2>
+          <p className="allTransactionDetails">
+            NGO id - #{props.ngoid}
+          </p>
+          <p className="allTransactionDetails">
+            Location - ₹ {props.location}
+          </p>
+          <p className="allTransactionDetails">Field - {props.field}</p>
+        </div>
       </div>
-    );
-  }
+      <span className="cardLine"></span>
+    </div>
+  );
 }
-
-export default withRouter(NgoListCard);
